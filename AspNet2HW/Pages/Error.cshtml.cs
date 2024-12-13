9ea345df8013d2bc1a5c439b660a931fa62b11a7
Grig_mi62 <Grig_mi62@student.itstep.org>
2024-12-13 19:34:31 +0200

    Change in error

--- AspNet2HW/Pages/Error.cshtml.cs
+++ AspNet2HW/Pages/Error.cshtml.cs
@@ -1,3 +1,5 @@
+using AspNet2HW.Abstract;
+using AspNet2HW.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
@@ -14,15 +16,42 @@ namespace AspNet2HW.Pages
 
         private readonly ILogger<ErrorModel> _logger;
 
+        private readonly IFiguresOutputService _figureOutputService;
+        public Figure Figure { get; set; }
+
+        public List<Figure> Figures { get; set; } = new List<Figure>();
+
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
+        }
+        
+        public ErrorModel(IFiguresOutputService figureOutputService)
+        {
+            _figureOutputService = figureOutputService;
         }
 
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+
+            Rectangle garry = new Rectangle()
+            {
+                Width = 15,
+                Height = 10
+            };
+
+
+
+            //_figureOutputService.SaveToFile(garry, "figure.txt");
+
+            Figures = _figureOutputService.LoadFigure("figure.txt");
+
+            Figures = _figureOutputService.LoadFigureFromJson("figure.json");
+            //_figureOutputService.SaveToFileJson(garry, "figure.json");
         }
+
+
     }
 
 }
